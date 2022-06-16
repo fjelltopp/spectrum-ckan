6644bb789a68305cfc418b545b5f3f7c353eb65e
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-06-16 10:20:29 +0100

    OHT-34 Add testing env config script

---
+++ test_env.sh
@@ -0,0 +1,6 @@
+export CKAN_SQLALCHEMY_URL=postgresql://ckan:12345678@192.168.49.2/ckan_test
+export CKAN_DATASTORE_WRITE_URL=postgresql://ckan:12345678@192.168.49.2/datastore_test
+export CKAN_DATASTORE_READ_URL=postgresql://ckan:12345678@192.168.49.2/datastore_test
+export CKAN_SOLR_URL=http://192.168.49.2:8983/solr/ckan
+export CKAN_REDIS_URL=redis://192.168.49.2:6379/1
+export CKAN_STORAGE_PATH=./build/ckan_storage
